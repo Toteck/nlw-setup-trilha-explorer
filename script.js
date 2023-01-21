36eceed86e65a1a26a27fa12e5055583c10dc195
mateuswdo <mateusweslley14@gmail.com>
2023-01-20 21:29:37 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
+
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+// (evento, funcao)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso!")
+    return
+  }
+  alert("Adicionado com sucesso!")
+  nlwSetup.addDay(today)
+}
+
+function save() {
+  localStorage.setItem("NLWsetup@habits", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWsetup@habits")) || {}
+
+nlwSetup.setData(data)
+nlwSetup.load()
+
+// localStorage é um objeto que guarda na memória no browser informações
+
+//addEventListener é o ouvidor de evento algo que vai ficar
+// ouvindo o evento
+
+// const data = {
+//   run: ["01-01", "01-02", "01-06", "01-07", "01-08"],
+//   takePills: ['01-03'],
+//   journal: ['01-02']
+// }
+
